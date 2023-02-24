53a480fc33df018678078fe6dca64c905190e5a1
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-02-24 10:44:13 +0200

    9.23

--- patientor/src/components/SinglePatient.tsx
+++ patientor/src/components/SinglePatient.tsx
@@ -1,7 +1,59 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
-import { Patient } from "../types"
+import { Entry, Patient } from "../types"
 import patientService from "../services/patients"
+
+
+interface EntryProps {
+  entry: Entry
+}
+
+const EntryComp = ({ entry }: EntryProps) => {
+  if (entry.diagnosisCodes) {
+    return (
+      <>
+        <p>{entry.date} {entry.description}</p>
+        <ul>
+          {entry.diagnosisCodes.map((code: string) =>
+            // We can use 'code' as the key as it will be unique among this list
+            // (There won't be two same diagnosis codes in the same entry)
+            <li key={code}>{code}</li>
+          )}
+        </ul>
+      </>
+    )
+  }
+
+  // If there are no diagnosis codes, simply return date and description
+  return (
+    <>
+      <p>{entry.date} {entry.description}</p>
+    </>
+  )
+
+}
+
+interface EntriesProps {
+  entries: Entry[]
+}
+
+const EntriesList = ({ entries }: EntriesProps) => {
+  // Don't render the entries portion if the patient has no entries
+  if (entries.length === 0) {
+    return null
+  }
+
+  return (
+    <>
+      <p><b>Entries:</b></p>
+      <ul>
+        {entries.map((entry: Entry) =>
+          <EntryComp key={entry.id} entry={entry} />
+        )}
+      </ul>
+    </>
+  )
+}
 
 
 const SinglePatient = () => {
@@ -19,6 +71,7 @@ const SinglePatient = () => {
   }, [id]);
 
 
+
   if (patient) {
     return (
       <>
@@ -26,6 +79,8 @@ const SinglePatient = () => {
         <p>Gender: {patient.gender}</p>
         <p>Occupation: {patient.occupation}</p>
         <p>SSN: {patient.ssn}</p>
+        <br></br>
+        <EntriesList entries={patient.entries} />
       </>
     )
   }
--- patientor/src/types.ts
+++ patientor/src/types.ts
@@ -13,10 +13,58 @@ export enum Gender {
 export interface Patient {
   id: string;
   name: string;
-  occupation: string;
+  dateOfBirth: string;
+  ssn: string;
   gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  occupation: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare"
+  employerName: string;
+  sickLeave?: {
+    startDate: string,
+    endDate: string
+  }
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital"
+  discharge: {
+    date: string,
+    criteria: string
+  }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
